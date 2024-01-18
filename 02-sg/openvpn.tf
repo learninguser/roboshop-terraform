638703f44a54c2ff766496169a07d8775ecb4f21
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-19 00:41:48 +0100

    Add openvpn port rule to SG

--- 02-sg/openvpn.tf
+++ 02-sg/openvpn.tf
@@ -5,14 +5,22 @@ module "vpn" {
   sg_description = "SG for vpn"
   vpc_id         = data.aws_vpc.default.id
   sg_name        = "vpn"
-  # sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
 
-resource "aws_security_group_rule" "vpn" {
-  security_group_id = module.mongodb.sg_id
+resource "aws_security_group_rule" "rule_1" {
+  security_group_id = module.vpn.sg_id
   type              = "ingress"
   from_port         = 22
   to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["77.64.254.132/32"]
+}
+
+resource "aws_security_group_rule" "rule_2" {
+  security_group_id = module.vpn.sg_id
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
   protocol          = "tcp"
   cidr_blocks       = ["77.64.254.132/32"]
 }

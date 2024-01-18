e7270365dc631e5acc9c77386e52823bce03192b
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-18 22:01:37 +0100

    Set enable peering to true

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,12 +1,13 @@
 module "roboshop" {
-  source = "../../terraform-aws-vpc"
+  source       = "../../terraform-aws-vpc"
   project_name = var.project_name
-  environment = var.environment
-  cidr_block = var.cidr_block
+  environment  = var.environment
+  cidr_block   = var.cidr_block
 
-  public_subnet_cidr = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
+  public_subnet_cidr   = var.public_subnet_cidr
+  private_subnet_cidr  = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
-  
-  common_tags = var.common_tags
+
+  common_tags         = var.common_tags
+  is_peering_required = true
 }

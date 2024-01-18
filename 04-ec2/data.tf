82a7fcaf63913b0cf0148cc040016742c81c5aea
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-18 14:20:01 +0100

    Fech devops-practice AMI ID and MongoDB SG ID

---
+++ 04-ec2/data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
+}

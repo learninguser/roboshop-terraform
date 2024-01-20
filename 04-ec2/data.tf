f1af761281146348ff36ba3ce0dc3aedff119c8b
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-21 00:06:26 +0100

    Fetch default VPC subnet ID in us-east-1a

--- 04-ec2/data.tf
+++ 04-ec2/data.tf
@@ -70,3 +70,12 @@ data "aws_ssm_parameter" "payment_sg_id" {
 data "aws_ssm_parameter" "web_sg_id" {
   name = "/${var.project_name}/${var.environment}/web_sg_id"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  vpc_id = data.aws_vpc.default.id
+  availability_zone = "us-east-1a"
+}

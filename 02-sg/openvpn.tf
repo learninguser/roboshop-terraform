822bbff4edec50fbc92ca28512099cc71083f38e
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-20 17:12:05 +0100

    Add VPN SG ID to all components SGs

--- 02-sg/openvpn.tf
+++ 02-sg/openvpn.tf
@@ -16,3 +16,84 @@ resource "aws_security_group_rule" "mongodb_vpn" {
   protocol                 = "tcp"
   security_group_id        = module.mongodb.sg_id
 }
+
+resource "aws_security_group_rule" "redis_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.redis.sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.rabbitmq.sg_id
+}
+
+resource "aws_security_group_rule" "mysql_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.mysql.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "user_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.user.sg_id
+}
+
+resource "aws_security_group_rule" "cart_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "shipping_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.shipping.sg_id
+}
+
+resource "aws_security_group_rule" "payment_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.payment.sg_id
+}
+
+resource "aws_security_group_rule" "web_vpn" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.web.sg_id
+}
